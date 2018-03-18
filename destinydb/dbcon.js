940f682c997e9132b833ed4c586e9ee5d90c94a3
wongnich <wongnich@gmail.com>
2018-03-17 22:17:23 -0700

    updating dbcon to work with jawsdb

--- destinydb/dbcon.js
+++ destinydb/dbcon.js
@@ -4,6 +4,6 @@ var pool = mysql.createPool({
   host            : 'qbct6vwi8q648mrn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   user            : 'm4bjcarc8cc3znnc',
   password        : 'o5n069h1kkl5tfd4',
-  port        	  : '3306'
+  database        : 'h4vefgtq1dh3mcwt'
 });
 module.exports.pool = pool;
--- destinydb/main.js
+++ destinydb/main.js
@@ -4,7 +4,7 @@
 */
 
 var express = require('express');
-var mysql = require('mysql.js');
+var mysql = require('./dbcon.js');
 var bodyParser = require('body-parser');
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
